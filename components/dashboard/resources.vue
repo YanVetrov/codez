a6d3102664f00619efe8b540cfb3a8c7b5e19661
yan <redchilik@gmail.com>
2018-05-28 14:09:31 +0000

    sort res

--- components/dashboard/resources.vue
+++ components/dashboard/resources.vue
@@ -6,9 +6,9 @@
                         <ul class="country-state  p-t-20">
                             <li v-for="country in Stat" :key="country.id">
                                 <h2>{{country.count}}</h2> <small>From {{country.url}}</small>
-                                <div class="pull-right">25% <i class="fa fa-level-up text-success"></i></div>
+                                <div class="pull-right">{{country.percent}}%<i class="fa fa-level-up text-success"></i></div>
                                 <div class="progress">
-                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" :style="{width:'25%'}"> <span class="sr-only"></span></div>
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" :style="{width:country.percent +'%'}"> <span class="sr-only"></span></div>
                                 </div>
                             </li>
   
@@ -39,12 +39,26 @@
               let obj={};
               let arr=[];
               main.forEach(el => {
-                  let date = el.date.slice(0,10);
                   el.fromUrl.forEach(el=>{
-                      el.date = date;
-                      arr.push(el)
+                      let count = el.count;
+                      let url = el.url;
+                      obj[url]?obj[url]=parseInt(obj[url])+count:obj[url]=count;
+                      
+                      
                   })
               });
+              let total=0;
+              for(let k in obj){
+                  let objj = {};
+                  objj.url = k;
+                  objj.count = obj[k];
+                  total+=obj[k];
+                  arr.push(objj);
+              }
+              arr.forEach(el=>{
+                 el.percent = Math.ceil((parseFloat(el.count * 100 / total))*10)/10
+              })
+              console.log(arr);
               this.Stat = arr;
              
             })
