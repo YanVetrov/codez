d82679ab28151a12f0e5f723b51d88e6816cc537
bogdanmedvedev <bogdan@medve-dev.net>
2018-06-13 15:13:26 +0300

    fix history

--- components/history/all.vue
+++ components/history/all.vue
@@ -1,26 +1,28 @@
 <template>
     <div class="row">
-              название операции
+        название операции
         <input type="text" placeholder="example: 412423423" name="heh" v-model="operName"/>
         <select v-model="currency">
             <option disabled>Валюта</option>
             <option>валюта 1</option>
         </select>
-        <button class="fcbtn btn btn-info btn-1b" @click="getAdminHistory" style="margin:5px"><i class='fa fa-search'></i></button>
-        
+        <button class="fcbtn btn btn-info btn-1b" @click="getAdminHistory" style="margin:5px"><i
+                class='fa fa-search'></i></button>
+
         <history :history="history"></history>
-         <paging
-          :currentPage="current_page"
-          :totalPages="total_page"
-          @page-changed="getAdminHistory"
-         />
-         
-        </div>
+        <paging
+                :currentPage="current_page"
+                :totalPages="total_page"
+                @page-changed="getAdminHistory"
+        />
+
+    </div>
 </template>
 
 <script>
     import paging from '~/components/pagination';
     import history from '~/components/history/history';
+
     export default {
         data() {
             return {
@@ -34,20 +36,20 @@
         },
         methods: {
             getAdminHistory(page) {
-                this.$root.$emit('loading', false);
-                let obj = {}
+                this.$root.$emit('loading', true);
+                let obj = {};
                 this.operName.trim() !== '' ? obj.operName = this.operName : '';
                 this.currency !== '' ? obj.currency = this.currency : '';
                 this.filterParam = obj;
                 this.$rest.api('getAdminHistory', obj)
                     .then(res => {
-                        console.log(res);
+                        this.$root.$emit('loading', false);
+
                         if (res.success) {
                             this.current_page = res.data.count.select_page || 1;
                             this.history = res.data.history;
                             this.total_page = res.data.count.pages;
-                            
-                            
+
                         }
                         else {
                             this.$notify({
@@ -57,10 +59,9 @@
                                 title: 'Error get history!',
                                 text: res.error.message,
                             });
-                           
+
                             this.$router.back();
                         }
-                        this.$root.$emit('loading', true);
 
                     })
             },
@@ -68,6 +69,9 @@
         components: {
             history,
             paging
+        },
+        mounted() {
+            return this.getAdminHistory();
         }
 
     }
