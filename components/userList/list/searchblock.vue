61aded1cd230a40082c606a226cfa4d5a3abcbf5
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-18 17:12:02 +0300

    fix search

--- components/userList/list/searchblock.vue
+++ components/userList/list/searchblock.vue
@@ -1,54 +1,33 @@
 <template>
-
-
-    
-        
-            
-           
-            
-                
-                <div class="search-block border">
-                    <ul>
-                
-                        <li class="search-block--input">
-                            
-                            
-                            <div>
-                                <button><span class="icon-search"></span></button>
-                                <input type="search"  @input='$emit("onenter", $event.target.value)' :placeholder="ph">
-                            </div>
-                            
-                            
-                        </li>
-                
-                        <li class="search-block--select">
-                            <div>
-                
-                                <span class="icon-filter" @click="_.random(20)"></span>
-                
-                                <div class="search-block--select__">
-                                    <select>
-                                        <option @click='$emit("onselect",1)'>Сначала новые</option>
-                                        <option @click='$emit("onselect",-1)'>Сначала старые</option>
-                                    </select>
-                                </div>
-                
-                            </div>
-                        </li>
-                
-                    </ul>
+    <div class="search-block border">
+        <ul>
+            <li class="search-block--input">
+                <div>
+                    <button><span class="icon-search"></span></button>
+                    <input type="search" @input='$emit("onenter", $event.target.value)' :placeholder="ph">
                 </div>
-            
-            
-               
+            </li>
+            <li class="search-block--select">
+                <div>
+                    <span class="icon-filter" @click="_.random(20)"></span>
+                    <div class="search-block--select__">
+                        <select>
+                            <option @click='$emit("onselect",1)'>Сначала новые</option>
+                            <option @click='$emit("onselect",-1)'>Сначала старые</option>
+                        </select>
+                    </div>
 
+                </div>
+            </li>
 
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        props: ['ph','classes'],
-        
+        props: ['ph', 'classes'],
+
     }
 </script>
 
