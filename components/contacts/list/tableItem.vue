16b42728d16cf3b1261387e72fc62bcbc5ae5342
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-26 01:32:05 +0300

    refactor v0.0.8

--- components/contacts/list/tableItem.vue
+++ components/contacts/list/tableItem.vue
@@ -1,72 +1,44 @@
 <template>
+    <tr :ident="user._id">
+        <td>
+            <span class="icon-company"><img src="~/assets/img/bank.svg" alt=""></span>
+        </td>
 
+        <td>
+            <p>{{user.name}}</p>
+        </td>
 
-    
-        
-            
-                
-                          <tr :ident="user._id">
-            
-                                <td>
-            
-                                    <span class="icon-company"><img src="~/assets/img/bank.svg" alt=""></span>
-            
-                                </td>
-            
-                                <td>
-                                    <p>{{user.name}}</p>
-                                </td>
-            
-                                <td>
-            
-                                   <div class="correct-input">
-            
-                                       <label>
-            
-                                           <input type="text" placeholder="нет контакта" v-model='user.value' @blur="$emit('edit',user)">
-            
-                                           <span class="btn btn__correct"></span>
-                                       </label>
-            
-            
-                                   </div>
-            
-            
-                                </td>
-            
-                                <td class="user-data">
-            
-                                    <div class="correct-input">
-            
-                                        <label>
-            
-                                            <input type="text" placeholder="нет ссылки" v-model='user.link' @blur="$emit('edit',user)">
-            
-                                            <span class="btn btn__correct"></span>
-            
-                                        </label>
-            
-                                    </div>
-            
-                                </td>
-            
-                                <td>
-                                    <p class="color green">25%</p>
-                                </td>
-            
-                                <td class="dragg">
-                                    <div class="table-btn-users">
-                                        <a  class="btn btn-simple cart-delete" @click.prevent="$emit('ondelete',user._id)"></a>
-                                    </div>
-                                    <div class="double-b"></div>
-                                </td>
-                            </tr>
+        <td>
 
-            
-            
-        
-       
+            <div class="correct-input">
+                <label>
+                    <input type="text" placeholder="нет контакта" v-model='user.value' @blur="$emit('edit',user)">
+                    <span class="btn btn__correct"></span>
+                </label>
+            </div>
 
+        </td>
+
+        <td class="user-data">
+            <div class="correct-input">
+                <label>
+                    <input type="text" placeholder="нет ссылки" v-model='user.link' @blur="$emit('edit',user)">
+                    <span class="btn btn__correct"></span>
+                </label>
+            </div>
+        </td>
+
+        <td>
+            <p class="color green">25%</p>
+        </td>
+
+        <td class="dragg">
+            <div class="table-btn-users">
+                <a class="btn btn-simple cart-delete" @click.prevent="$emit('ondelete',user._id)"></a>
+            </div>
+            <div class="double-b"></div>
+        </td>
+    </tr>
 
 
 </template>
@@ -79,9 +51,10 @@
 
 <style lang='scss' scoped>
     .double-b {
-	cursor: -webkit-grabbing;
-}
-    .ghost{
-        opacity:0.9;
+        cursor: -webkit-grabbing;
+    }
+
+    .ghost {
+        opacity: 0.9;
     }
 </style>
