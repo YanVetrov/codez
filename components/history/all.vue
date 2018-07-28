9b3eb92d12d312454658556593fb51d62cfc9fdb
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-28 13:30:26 +0300

    refactor & modals & save locale

--- components/history/all.vue
+++ components/history/all.vue
@@ -1,30 +1,410 @@
 <template>
-    <div class="row">
-        {{$t('operation')}}
-        <input type="text" placeholder="example: 412423423" name="heh" v-model="operName"/>
-        <select v-model="currency">
-            <option disabled>{{$t('currency')}}</option>
-            <option>{{$t('currency')}} 1</option>
-        </select>
-        <button class="fcbtn btn btn-info btn-1b" @click="getAdminHistory" style="margin:5px"><i
-                class='fa fa-search'></i></button>
-        <div class='white-box'>
-            <history :history="history"></history>
-            <pagination
-                    :currentPage="current_page"
-                    :totalPages="total_page"
-                    @page-changed="getAdminHistory"
-            />
+    <div class="action-wr">
+        <div class="action-main border">
+
+            <div class="action-main--top">
+
+                <h3 class="title">ЗАЯВКАМИ на обмен</h3>
+
+            </div>
+
+            <search placeholder="Search text..." :filter="filterProps" ></search>
+
+
+            <div class="table table-head">
+
+                <table>
+
+                    <thead>
+                    <tr>
+
+                        <td class="small">
+                            <span>Data</span>
+                        </td>
+                        <td class="small"></td>
+                        <td></td>
+                        <td class="user">
+                            <span>USER</span>
+                        </td>
+                        <td class="middle">
+                            <span>IP INFO</span>
+                        </td>
+
+                    </tr>
+                    </thead>
+
+                </table>
+
+            </div>
+
+            <div class="table">
+
+                <table>
+
+
+                    <tbody>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    <tr>
+
+                        <td class="small">
+
+                            <p class="time">14:36</p>
+                            <span>12.03,18</span>
+
+                        </td>
+
+                        <td class="small">
+
+                            <span class="icon-company img-border"><img src="img/bank.svg" alt=""></span>
+
+                        </td>
+
+                        <td class="bg bg-ar">
+
+                            <div class="df">
+
+                                <p class="name">Вася Пупкин <i class="fal fa-angle-right"></i></p>
+                                <p>Создал новость “Курс биткойна ростет!”</p>
+
+                            </div>
+
+                        </td>
+
+                        <td class="bg user">
+
+                            <div>
+
+                                <span class="icon-company"><img src="img/bank.svg" alt=""></span>
+
+                                <div class="company">
+                                    <p class="name-company">Qiwi Wallet</p>
+                                    <p><span>support@mail.ru</span></p>
+                                </div>
+
+                            </div>
+
+                        </td>
+
+
+                        <td class="bg middle">
+
+                            <div class="ip">
+
+                                <span class="lang"><img src="img/sadness-face.svg" alt=""></span>
+                                <p>218.134.155.43</p>
+                                <span class="icon-laptop"></span>
+
+                            </div>
+
+                        </td>
+
+                    </tr>
+
+                    </tbody>
+
+                </table>
+                <pagination
+                        :currentPage="current_page"
+                        :totalPages="total_page"
+                        @page-changed="getAdminHistory"
+                />
+
+            </div>
+
         </div>
     </div>
+
+
 </template>
 
 <script>
+    import search from '~/components/_utils/search';
     import history from '~/components/history/history';
 
     export default {
         data() {
             return {
+                filterProps: [{name:'Все типы',value:'all'},{name:'Авторизации',value:'auth'}],
                 operName: '',
                 currency: '',
                 current_page: '',
@@ -65,7 +445,7 @@
                     })
             },
         },
-        components: {history},
+        components: {history, search},
         mounted() {
             return this.getAdminHistory();
         }
