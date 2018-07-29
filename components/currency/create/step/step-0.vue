f0a8762687ff13eb6fef11ff2d4a8c2464c890c0
bogdanmedvedev <bogdan@medve-dev.net>
2018-07-29 16:59:05 +0300

    create currency (fix)

---
+++ components/currency/create/step/step-0.vue
@@ -0,0 +1,104 @@
+<template>
+
+    <div class="create-currency-sidebar">
+
+        <h4 class="create-currency-sidebar--title text-center">Логотип системы</h4>
+
+        <!--<p><img src="#"/></p>-->
+
+        <div class="create-currency-sidebar__bot" style="    text-align: center;">
+
+
+            <div>
+                <p>Логотип валюты должно быть не меньше чем <b>100 x 100 px</b> и иметь формат: <b>PNG,JPG,JPEG</b></p>
+            </div>
+        </div>
+
+        <ul class="create-currency-sidebar--tools">
+            <li>
+                <button class="btn"><i class="fal fa-cloud-upload"></i> Загрузить лого</button>
+            </li>
+            <!--  <li>
+                  <button class="btn"><i class="fal fa-times-circle"></i> Удалить</button>
+              </li>-->
+        </ul>
+
+        <h4 class="create-currency-sidebar--title text-center">Основные данные</h4>
+
+        <div class="create-currency-sidebar__main">
+
+            <div class="create-currency-sidebar__item">
+
+                <span><i class="fal fa-file-signature" style="color: #4584f6;"></i></span>
+
+                <div class="create-currency-sidebar__item--info">
+                    <span>Платежная система</span>
+                    <p>Alfa bank LLC</p>
+                </div>
+
+            </div>
+
+            <div class="create-currency-sidebar__item">
+
+                <span><i class="fal fa-usd-circle" style="font-size: 24px;    color: #4584f6;"></i></span>
+
+                <div class="create-currency-sidebar__item--info">
+                    <span>Тип валюты</span>
+                    <p>USD</p>
+                </div>
+
+            </div>
+
+            <div class="create-currency-sidebar__item">
+
+                <span><i class="fal fa-code" style="color: #4584f6;"></i></span>
+
+                <div class="create-currency-sidebar__item--info">
+                    <span>XML валюты</span>
+
+                    <p>
+
+                        <select>
+                            <option>Выбрать</option>
+                            <option>Выбрать 1</option>
+                            <option>Выбрать 2</option>
+                        </select>
+
+                        <i class="fal fa-sort-down"></i>
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="create-currency-sidebar__bot">
+
+            <i class="fal fa-comment-alt-exclamation"></i>
+
+            <div>
+                <p>XML обозначение согласно стандарту</p>
+                <span><a href="http://estandards.info/formirovanie-eksportnogo-fajla-s-kursami/" target="_blank">estandards.info</a></span>
+            </div>
+        </div>
+
+        <div class="create-currency-sidebar__next">
+            <button class="btn btn-blue" @click="$emit('saveStep0')" v-if="step === 0">продолжить далее</button>
+        </div>
+
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['data', 'step'],
+        data() {
+            return {}
+        },
+
+        methods: {}
+    }
+</script>
