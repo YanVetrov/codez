9fadd34404d7e41caac9663c616c2e343c028d28
bogdanmedvedev <www.clsa.ru@gmail.com>
2018-09-02 19:55:14 +0300

    footer

---
+++ components/_utils/footer/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <footer class="footer text-center">
+        <span style="float: left;width: 45%;">ProExchanger.net All rights reserved.</span>
+        <ul class="list-unstyled">
+            <li>
+                <span class="fab fa-telegram" style="color: #0b96ff;font-size: 15px;"></span>
+                <a href="https://t.me/ProExchanger" target="_blank">ProExchanger</a>
+            </li>
+            <li>
+                <span class="fab fa-skype" style="color: #0b96ff;font-size: 15px;"></span>
+                <a href="skype:bogdan.medve_dev">bogdan.medve_dev</a>
+            </li>
+            <li>
+                <span class="fab fa-github" style="color: #2b2b2b;font-size: 15px;"></span>
+                <a href="https://github.com/medve-dev">medve-dev</a>
+            </li>
+        </ul>
+    </footer>
+</template>
