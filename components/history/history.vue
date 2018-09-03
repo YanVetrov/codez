21d2c6c0fe6c69c803e6c2a9977b79cf35a5f041
bogdanmedvedev <www.clsa.ru@gmail.com>
2018-09-04 00:29:53 +0300

    history maps

--- components/history/history.vue
+++ components/history/history.vue
@@ -5,8 +5,8 @@
                    v-for="(action,i) in history"
                    :method="history.method"
                    :date="action.createdAt"
-                   :param="action.param || 'smeh'"
-                   :user="action.user || 'kolya'"
+                   :param="action.param"
+                   :user="action.user"
                    :network="action.network"
                    :response="action.response"
                    
@@ -46,35 +46,35 @@
         data() {
             return {
                 type: {
-                    deleteCommentNews: {componentName: 'actionCommentForNews', icon: 'exchange'},
+                    "admin/news/comment/delete": {componentName: 'actionCommentForNews', icon: 'exchange'},
 
-                    createNews: {componentName: "actionNews", icon: 'sign-in'},
-                    editNews: {componentName: "actionNews", icon: 'sign-in'},
-                    deleteNews: {componentName: "actionNews", icon: 'sign-in'},
+                    "admin/news/create": {componentName: "actionNews", icon: 'sign-in'},
+                    "admin/news/edit": {componentName: "actionNews", icon: 'sign-in'},
+                    "admin/news/delete": {componentName: "actionNews", icon: 'sign-in'},
 
-                    createRules: {componentName: "actionRules", icon: 'sign-in'},
-                    editRules: {componentName: "actionRules", icon: 'sign-in'},
-                    deleteRule: {componentName: "actionRules", icon: 'sign-in'},
+                    "admin/rule/create": {componentName: "actionRules", icon: 'sign-in'},
+                    "admin/rule/edit": {componentName: "actionRules", icon: 'sign-in'},
+                    "admin/rule/delete": {componentName: "actionRules", icon: 'sign-in'},
 
-                    createPartner: {componentName: "actionPartner", icon: 'sign-in'},
-                    deletePartner: {componentName: "actionPartner", icon: 'sign-in'},
+                    "admin/partners-block/create": {componentName: "actionPartner", icon: 'sign-in'},
+                    "admin/partners-block/delete": {componentName: "actionPartner", icon: 'sign-in'},
 
-                    createFaq: {componentName: "actionFaq", icon: 'sign-in'},
-                    editFaq: {componentName: "actionFaq", icon: 'sign-in'},
-                    deleteFaq: {componentName: "actionFaq", icon: 'sign-in'},
+                    "admin/faq/create": {componentName: "actionFaq", icon: 'sign-in'},
+                    "admin/faq/edit": {componentName: "actionFaq", icon: 'sign-in'},
+                    "admin/faq/delete": {componentName: "actionFaq", icon: 'sign-in'},
 
-                    addContact: {componentName: "actionContact", icon: 'sign-in'},
-                    editContact: {componentName: "actionContact", icon: 'sign-in'},
-                    deleteContact: {componentName: "actionContact", icon: 'sign-in'},
+                    "admin/contact/create": {componentName: "actionContact", icon: 'sign-in'},
+                    "admin/contacts/edit": {componentName: "actionContact", icon: 'sign-in'},
+                    "admin/contacts/delete": {componentName: "actionContact", icon: 'sign-in'},
 
-                    confirmReviewAdmin: {componentName: "actionReview", icon: 'sign-in'},
-                    editReviewAdmin: {componentName: "actionReview", icon: 'sign-in'},
+                    // confirmReviewAdmin: {componentName: "actionReview", icon: 'sign-in'},
+                    // editReviewAdmin: {componentName: "actionReview", icon: 'sign-in'},
 
-                    editUser: {componentName: "actionUser", icon: 'sign-in'},
+                    "admin/users/profile/edit": {componentName: "actionUser", icon: 'sign-in'},
 
-                    createCurrency: {componentName: "actionCurrency", icon: 'sign-in'},
-                    editCurrency: {componentName: "actionCurrency", icon: 'sign-in'},
-                    deleteCurrency: {componentName: "actionCurrency", icon: 'sign-in'},
+                    "admin/exchanger/currency/create": {componentName: "actionCurrency", icon: 'sign-in'},
+                    "admin/exchanger/currency/edit": {componentName: "actionCurrency", icon: 'sign-in'},
+                    "admin/exchanger/currency/delete": {componentName: "actionCurrency", icon: 'sign-in'},
                 }
             }
         },
@@ -93,70 +93,3 @@
     }
 </script>
 
-
-<!--<style>-->
-    <!--.history-block {-->
-        <!--color: #666;-->
-        <!--width: 100%;-->
-        <!--display: flex;-->
-        <!--flex-direction: row;-->
-        <!--margin-top: 15px;-->
-    <!--}-->
-
-    <!--.history-info {-->
-        <!--border-radius: 3px;-->
-        <!--width: 95%;-->
-        <!--display: flex;-->
-        <!--flex-direction: column;-->
-        <!--background: rgba(245, 245, 245, 1);-->
-        <!--padding: 15px;-->
-
-    <!--}-->
-
-    <!--.history-title {-->
-        <!--display: flex;-->
-        <!--flex-direction: row;-->
-        <!--justify-content: space-between;-->
-        <!--border-bottom: 1px solid #CCC;-->
-        <!--padding: 5px;-->
-
-    <!--}-->
-
-    <!--.history-operation {-->
-        <!--padding: 5px;-->
-
-    <!--}-->
-
-    <!--.history-icon {-->
-        <!--display: flex;-->
-        <!--justify-content: center;-->
-        <!--align-items: center;-->
-        <!--width: 5%;-->
-    <!--}-->
-
-    <!--.date {-->
-        <!--padding: 15px;-->
-        <!--position: relative;-->
-        <!--top: -50%;-->
-        <!--font-size: 8px;-->
-    <!--}-->
-
-    <!--.timeline::before {-->
-        <!--left: 15px;-->
-    <!--}-->
-
-    <!--.timeline > li.timeline-inverted > .timeline-panel {-->
-        <!--float: none;-->
-        <!--left: 50px;-->
-    <!--}-->
-
-    <!--.timeline > li > .timeline-panel {-->
-        <!--float: none;-->
-        <!--position: relative;-->
-        <!--width: 100%;-->
-    <!--}-->
-
-    <!--.timeline > li > .timeline-badge {-->
-        <!--left: 15px;-->
-    <!--}-->
-<!--</style>-->
