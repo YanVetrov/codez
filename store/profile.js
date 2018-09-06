b6f4827f82bdd04a0ddb97a86725694c58d904d8
bogdanmedvedev <www.clsa.ru@gmail.com>
2018-09-07 01:31:57 +0300

    fix id->userId editUser

--- store/profile.js
+++ store/profile.js
@@ -23,9 +23,9 @@ export const actions = {
         await console.log(profile.data)
     },
     async editProfile({ commit, dispatch }, obj) {
-        obj.id = obj._id
-        obj.rate = obj.affiliate.rate
-        console.log(obj)
+        obj.userId = obj._id;
+        obj.rate = obj.affiliate.rate;
+        console.log(obj);
         let res = await this.app.$rest.api('admin/users/profile/edit', obj)
         console.log(res)
     },
