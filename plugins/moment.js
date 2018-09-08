6e7ea9fa7b79cdaec7ef8529f6a701088e1e783f
bogdanmedvedev <www.clsa.ru@gmail.com>
2018-09-08 19:13:53 +0300

    fix style history

--- plugins/moment.js
+++ plugins/moment.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import moment from 'moment';
 import VueMoment from 'vue-moment';
-// require('moment/locale/ru');
+
 import 'moment/locale/fr'
 import 'moment/locale/ru'
 import 'moment/locale/en-gb'
